2f08f338fe0a7668dcd007d60aced89269cf09e1
Md. Mostofa Hasib <90277109+Hasib2202@users.noreply.github.com>
2026-03-04 12:53:43 +0600

    Add Category management service with CRUD operations and update proxy files

--- angular/src/app/proxy/index.ts
+++ angular/src/app/proxy/index.ts
@@ -1,3 +1,4 @@
 import * as Authors from './authors';
 import * as Books from './books';
-export { Authors, Books };
+import * as Categories from './categories';
+export { Authors, Books, Categories };
